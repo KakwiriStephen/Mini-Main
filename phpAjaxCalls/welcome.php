dd951423024bffdc8e6857bf94995ebc84484dd6
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-01 18:34:39 +0300

    Adding Bootstrap

---
+++ phpAjaxCalls/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Search User</title>
+    <link rel="stylesheet" href="http://bootswatch.com/cerulean/bootstrap.min.css">
+</head>
+
+<body>
+
+    <?php
+echo "My first PHP script!";
+?>
+
+</body>
+
+</html>
