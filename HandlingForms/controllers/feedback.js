6fd1c57572ea319122ab7cdc195f866870c044e6
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-08 17:50:13 +0300

    GET

--- HandlingForms/controllers/feedback.js
+++ HandlingForms/controllers/feedback.js
@@ -1,1 +1,10 @@
 const Feedback = require("../models/feedback");
+
+//Find a feedback GET
+
+exports.getFeedback = (req, res, next) {
+    Feedback.find().then(feedback => {
+            res.json(feedback)
+        })
+        .catch(err => next(err));
+};
