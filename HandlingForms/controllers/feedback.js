9330bcdcb7520054f2aeb75e2286f485f4dd69f1
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-08 18:28:06 +0300

    Create Feedback

--- HandlingForms/controllers/feedback.js
+++ HandlingForms/controllers/feedback.js
@@ -41,35 +41,9 @@ exports.getFeedback = (req, res) => {
     }
 };
 
-/*const flights = require("./../models/Flight");
-
-
-
-// get one flight--get
-e
-
-//create a flight-- post
-exports.createFlight = (req, res) => {
-    try {
-        let newId = flights[flights.length - 1].id + 1;
-        let newFlight = Object.assign({ id: newId }, req.body);
-        const { location, title, time, price, date } = req.body;
-        flights.push({
-            id: newId,
-            location: location,
-            title: title,
-            time: time,
-            price: price,
-            date: date,
-        });
-
-        res.status(201).json({
-            status: "success",
-            data: {
-                flights: newFlight,
-            },
-        });
-    } catch (err) {
+//create a Feedback- POST
+exports.createFeedback = (req, res) => {
+    try {} catch (err) {
         res.status(400).json({
             status: "fail",
             message: err,
@@ -77,40 +51,17 @@ exports.createFlight = (req, res) => {
     }
 };
 
-//update flight--patch
-exports.updateFlight = (req, res) => {
-    try {
-        const flightId = req.params.id;
-        const { location, title, time, price, date } = req.body;
+/*const flights = require("./../models/Flight");
 
-        flights = flights.map((flight) => {
-            if (flight.id === flightId) {
-                return {
-                    id: flight.id,
-                    location,
-                    title,
-                    time,
-                    price,
-                    date,
-                };
-            }
-            return flight;
-        });
-        // res.json(flights)
-        res.status(200).json({
-            status: "success",
-            results: flights.length,
-            data: {
-                flights,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+
+
+// get one flight--get
+e
+
+
+
+//update flight--patch
+
 
 //delete flight
 exports.deleteFlight = (req, res) => {
