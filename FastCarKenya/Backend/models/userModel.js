7cc06f6c95f6ef56c44d4f716b05be650044d96e
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-17 12:33:27 +0300

    Users

---
+++ FastCarKenya/Backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: user,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Users", userSchema);
