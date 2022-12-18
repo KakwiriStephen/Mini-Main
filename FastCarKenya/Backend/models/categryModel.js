17ce1ccae53254fdab7b2d4697d54ead18226703
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-18 14:21:41 +0300

    FastCar

---
+++ FastCarKenya/Backend/models/categryModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  parentId: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
