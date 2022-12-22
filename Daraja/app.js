a426cbf740400ec186c4f4c04cc1054b72c44225
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-22 18:18:00 +0300

    unirest

--- Daraja/app.js
+++ Daraja/app.js
@@ -21,21 +21,21 @@ app.use((req, res, next) => {
   next();
 });
 
-var req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate");
+// var req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate");
 
-req.query({
-  grant_type: "client_credentials",
-});
+// req.query({
+//   grant_type: "client_credentials",
+// });
 
-req.headers({
-  Authorization:
-    "Basic SWZPREdqdkdYM0FjWkFTcTdSa1RWZ2FTSklNY001RGQ6WUp4ZVcxMTZaV0dGNFIzaA==",
-});
+// req.headers({
+//   Authorization:
+//     "Basic SWZPREdqdkdYM0FjWkFTcTdSa1RWZ2FTSklNY001RGQ6WUp4ZVcxMTZaV0dGNFIzaA==",
+// });
 
-req.end((res) => {
-  if (res.error) throw new Error(res.error);
-  console.log(res.body);
-});
+// req.end((res) => {
+//   if (res.error) throw new Error(res.error);
+//   console.log(res.body);
+// });
 
 // app.get("/access_token", (req, res) => {
 //   //Acces token
