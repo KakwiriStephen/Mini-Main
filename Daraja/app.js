ab5c11794311696bb18fbcf58535d17af69f0057
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-31 22:10:29 +0300

    Daraja

--- Daraja/app.js
+++ Daraja/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-// cors error
+// Handling cors error
 app.use((req, res, next) => {
   res.header("Access-Controll-Allow-Origin", "*");
   res.header(
